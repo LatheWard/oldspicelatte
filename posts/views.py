69058c9416b5b4a76117653a5b99824462a6e031
LatheWard <72397286+LatheWard@users.noreply.github.com>
2021-02-19 16:44:10 -0600

    Added spacing

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,5 @@ from django.views.generic import ListView
 from .models import Post
 # Create your views here.
 class PostListView(ListView):
-    model=Post
-    template_name='home.html'
+    model = Post
+    template_name = 'home.html'
