367db3655fe04a8ed292921c342ea398ec27164a
LatheWard <72397286+LatheWard@users.noreply.github.com>
2021-02-19 16:39:02 -0600

    Made post model, about to write view for it

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(model.Models):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE,)
+    text = models.TextField()
